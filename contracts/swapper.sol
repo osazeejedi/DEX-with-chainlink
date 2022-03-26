930814b6d8bd4cc7074ae4f28ea3f9f6f6460ee5
osazeejedi <oghagbonosazee20@gmail.com>
2022-03-26 18:29:56 +0100

    .

--- contracts/swapper.sol
+++ contracts/swapper.sol
@@ -9,7 +9,12 @@ contract Swapper {
 
     AggregatorV3Interface internal priceFeed;
     int public exchangePrice;
-    uint96 public decimals;
+    struct Order{uint256 amountIn;address owner;}
+    uint internal Index = 1;
+    mapping(uint=> Order) public swaps;
+
+    
+
 
     constructor() {
         priceFeed = AggregatorV3Interface(0x4746DeC9e833A82EC7C2C1356372CcF2cfcD2F3D);
@@ -30,19 +35,29 @@ contract Swapper {
     
 
     function DaiToUsdt (address _fromToken,address _toToken,uint _amountIn) internal{
-        uint rate = uint256(exchangePrice)/decimals;
+        uint rate = uint256(exchangePrice);
         uint swappedAmount = _amountIn * rate;
         require ( IERC20(_toToken).balanceOf(address(this))>= swappedAmount , "Insufficent funds");
         require(IERC20(_fromToken).transferFrom(msg.sender, address(this), _amountIn));
+        Order storage s= swaps[swapIndex];
+        s.amountIn= _amountIn;
+        s.owner=msg.sender;
+        swapIndex++;
+        IERC20(_toToken).transferFrom(address(this), msg.sender, (swappedAmount));
         
     }
 
 
     function UsdtToDai(address _fromToken,address _toToken,uint _amountIn) internal{
-        uint rate = uint256(exchangePrice)/decimals;
+        uint rate = uint256(exchangePrice);
         uint swappedAmount = (_amountIn*100000)/rate;
         require ( IERC20(_toToken).balanceOf(address(this))>= swappedAmount, "Insufficent funds");
         require(IERC20(_fromToken).transferFrom(msg.sender,address(this), _amountIn));
+        Order storage s= swaps[swapIndex];
+        s.amountIn= _amountIn;
+        s.owner=msg.sender;
+        swapIndex++;
+        IERC20(_toToken).transferFrom(address(this), msg.sender, swappedAmount/100000);
         
     }
 }
